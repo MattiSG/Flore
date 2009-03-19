d2009dd8ea4cd2015690ccbc4c05192f5ad146ed
RomainGiraud <giraud.romain@gmail.com>
2009-03-19 23:51:39 +0000

    Remplacement du BorderLayout par un GridBagLayout
    git-svn-id: file:///Users/matti/Desktop/tmp-1/flore/svn/trunk@8 0452f256-4c2a-4938-993f-bf7dc213af01

--- src/game/MainWindow.java
+++ src/game/MainWindow.java
@@ -14,16 +14,20 @@ import javax.swing.ImageIcon;
 import javax.swing.DefaultListModel;
 import javax.swing.ListSelectionModel;
 import javax.swing.JOptionPane;
+import java.awt.Insets;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 import java.awt.CardLayout;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class MainWindow extends JFrame {
     private JMenuBar     menuBar   = new JMenuBar();
     private JLabel       statusBar = new JLabel("Initialisation");
-    private JProgressBar levelBar  = new JProgressBar(JProgressBar.VERTICAL, 0, 20);
+    private JProgressBar levelBar  = new JProgressBar(JProgressBar.VERTICAL, 0, 100);
     private GameView     gameView  = new GameView();
     private DocView      docView   = new DocView();
     private JToggleButton    docButton    = new JToggleButton("Documentation", new ImageIcon("../ressources/images/doc.png"));
@@ -31,6 +35,7 @@ public class MainWindow extends JFrame {
     private JList            seedListView = new JList(seedList);
     private JPanel           centerPanel  = new JPanel();
     private CardLayout       centerLayout = new CardLayout();
+    private ArrayList<Dimension> trous = new ArrayList<Dimension>();
 
     public MainWindow() {
         setMenu();
@@ -44,19 +49,52 @@ public class MainWindow extends JFrame {
             }
         });
 
-        JPanel leftPanel = new JPanel();
-        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
-        leftPanel.add(docButton);
-        leftPanel.add(levelBar);
+        levelBar.setValue(50);
 
         centerPanel.setLayout(centerLayout);
         centerPanel.add(gameView, "game");
         centerPanel.add(docView,  "doc");
 
-        getContentPane().add(statusBar, BorderLayout.SOUTH);
-        getContentPane().add(leftPanel, BorderLayout.WEST);
-        getContentPane().add(new JScrollPane(seedListView),  BorderLayout.EAST);
-        getContentPane().add(centerPanel, BorderLayout.CENTER);
+
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(2, 2, 2, 2);
+        c.gridwidth  = 1;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.BOTH;
+
+        setLayout(layout);
+
+        layout.setConstraints(docButton, c);
+        getContentPane().add(docButton);
+
+        c.gridwidth  = 2;
+        c.gridheight = 3;
+        c.weightx = 1;
+        c.weighty = 1;
+        layout.setConstraints(centerPanel, c);
+        getContentPane().add(centerPanel);
+
+        c.gridwidth  = GridBagConstraints.REMAINDER;
+        c.gridheight = 3;
+        c.weightx = 0;
+        c.weighty = 0;
+        JScrollPane s = new JScrollPane(seedListView);
+        layout.setConstraints(s, c);
+        getContentPane().add(s);
+
+        c.gridy = 1;
+        c.gridx = 0;
+        c.gridwidth  = 1;
+        c.gridheight = 2;
+        layout.setConstraints(levelBar, c);
+        getContentPane().add(levelBar);
+
+        c.gridy = 3;
+        c.gridwidth  = GridBagConstraints.REMAINDER;
+        c.gridheight = 1;
+        layout.setConstraints(statusBar, c);
+        getContentPane().add(statusBar);
 
         setJMenuBar(menuBar);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -69,8 +107,9 @@ public class MainWindow extends JFrame {
     private void run() {
         int delay = 1000;
         ActionListener taskPerformer = new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
+            public void actionPerformed(ActionEvent e) {
                 // fait grandir ta plante ici !
+                statusBar.setText("tick: " + e.getWhen());
             }
         };
         new Timer(delay, taskPerformer).start();
