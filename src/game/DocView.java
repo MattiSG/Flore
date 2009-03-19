dd7134af9b8db21a44a2bcc39882b7f9b1094f11
RomainGiraud <giraud.romain@gmail.com>
2009-03-19 18:38:11 +0000

    Base pour la documentation.
    git-svn-id: file:///Users/matti/Desktop/tmp-1/flore/svn/trunk@3 0452f256-4c2a-4938-993f-bf7dc213af01

---
+++ src/game/DocView.java
@@ -0,0 +1,11 @@
+package game;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Component;
+
+public class DocView extends JPanel {
+    public DocView() {
+        add(new JLabel("C'est l'histoire de Toto..."));
+    }
+}
--- src/game/MainWindow.java
+++ src/game/MainWindow.java
@@ -1,52 +1,83 @@
 package game;
 
+import javax.swing.Timer;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JButton;
+import javax.swing.JToggleButton;
 import javax.swing.JMenuBar;
 import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.DefaultListModel;
-import java.awt.BorderLayout;
+import javax.swing.ListSelectionModel;
+import javax.swing.JOptionPane;
 import java.awt.Dimension;
+import java.awt.CardLayout;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindow extends JFrame {
     private JMenuBar     menuBar   = new JMenuBar();
     private JLabel       statusBar = new JLabel("Initialisation");
     private JProgressBar levelBar  = new JProgressBar(JProgressBar.VERTICAL, 0, 20);
-    private JButton      docButton = new JButton("Documentation", new ImageIcon("../ressources/images/doc.png"));
     private GameView     gameView  = new GameView();
-    private DefaultListModel seedList      = new DefaultListModel();
-    private JList            seedListView  = new JList(seedList);
+    private DocView      docView   = new DocView();
+    private JToggleButton    docButton    = new JToggleButton("Documentation", new ImageIcon("../ressources/images/doc.png"));
+    private DefaultListModel seedList     = new DefaultListModel();
+    private JList            seedListView = new JList(seedList);
+    private JPanel           centerPanel  = new JPanel();
+    private CardLayout       centerLayout = new CardLayout();
 
     public MainWindow() {
         setMenu();
         setSeedList();
 
-        docButton.setVerticalTextPosition(JButton.BOTTOM);
-        docButton.setHorizontalTextPosition(JButton.CENTER);
+        docButton.setVerticalTextPosition(JToggleButton.BOTTOM);
+        docButton.setHorizontalTextPosition(JToggleButton.CENTER);
+        docButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                centerLayout.next(centerPanel);
+            }
+        });
 
         JPanel leftPanel = new JPanel();
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
         leftPanel.add(docButton);
         leftPanel.add(levelBar);
 
+        centerPanel.setLayout(centerLayout);
+        centerPanel.add(gameView, "game");
+        centerPanel.add(docView,  "doc");
+
         getContentPane().add(statusBar, BorderLayout.SOUTH);
         getContentPane().add(leftPanel, BorderLayout.WEST);
         getContentPane().add(new JScrollPane(seedListView),  BorderLayout.EAST);
-        getContentPane().add(gameView,  BorderLayout.CENTER);
+        getContentPane().add(centerPanel, BorderLayout.CENTER);
 
         setJMenuBar(menuBar);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setFullScreen();
         setVisible(true);
+
+        run();
+    }
+
+    private void run() {
+        int delay = 1000;
+        ActionListener taskPerformer = new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                // fait grandir ta plante ici !
+            }
+        };
+        new Timer(delay, taskPerformer).start();
     }
 
     private void setSeedList() {
+        seedListView.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         seedList.addElement("Graîne de rosier");
         seedList.addElement("Graîne de mimosa");
     }
