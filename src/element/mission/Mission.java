c885792bf26720eeb1bb359d84e7abc9feec1726
MattiSG <matti.schneiderghibaudo@gmail.com>
2009-03-23 22:32:26 +0000

    Ajout du scénario de Clément
    git-svn-id: file:///Users/matti/Desktop/tmp-1/flore/svn/trunk@23 0452f256-4c2a-4938-993f-bf7dc213af01

--- src/element/mission/Mission.java
+++ src/element/mission/Mission.java
@@ -5,4 +5,9 @@ import element.Element;
 public class Mission extends Element {
     public Mission() {
     }
+	
+	protected String[] getAssetsNames() {
+		String[] tmp = {"", ""}; //w00t Java...
+		return tmp;
+	}
 }
