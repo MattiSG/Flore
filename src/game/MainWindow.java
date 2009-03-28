6669eecbd9cbc943c412d826a92b5422726a0f8c
RomainGiraud <giraud.romain@gmail.com>
2009-03-28 16:09:12 +0000

    Premières modifications de l'interface (plus claire)
    git-svn-id: file:///Users/matti/Desktop/tmp-1/flore/svn/trunk@39 0452f256-4c2a-4938-993f-bf7dc213af01

--- src/game/MainWindow.java
+++ src/game/MainWindow.java
@@ -5,30 +5,29 @@ import element.plant.Plant;
 import element.creature.Creature;
 import element.mission.Mission;
 
+import t2s.SIVOXDevint;
+
 import javax.swing.Timer;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JToggleButton;
 import javax.swing.JMenuBar;
 import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.DefaultListModel;
 import javax.swing.ListSelectionModel;
 import javax.swing.DefaultListCellRenderer;
-import javax.swing.JOptionPane;
-import java.awt.Insets;
+
 import java.awt.Dimension;
 import java.awt.Component;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.CardLayout;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import java.util.ArrayList;
 
@@ -36,15 +35,10 @@ public class MainWindow extends JFrame {
     private JMenuBar         menuBar       = new JMenuBar();
     private JLabel           statusBar     = new JLabel("Initialisation");
     private JProgressBar     levelBar      = new JProgressBar(JProgressBar.VERTICAL, 0, 100);
-    private GameView         gameView      = new GameView();
-    private DocView          docView       = new DocView();
-    private MissionView      missionView   = new MissionView();
-    private JToggleButton    docButton     = new JToggleButton("Documentation", new ImageIcon("../ressources/images/doc.png"));
-    private JToggleButton    missionButton = new JToggleButton("Mission", new ImageIcon("../ressources/images/doc.png"));
     private DefaultListModel seedList      = new DefaultListModel();
     private JList            seedListView  = new JList(seedList);
-    private JPanel           centerPanel   = new JPanel();
-    private CardLayout       centerLayout  = new CardLayout();
+    private GameView         gameView      = new GameView();
+    private SIVOXDevint      player        = new SIVOXDevint();
 
     private ArrayList<Plant>    plants   = new ArrayList<Plant>();
     private ArrayList<Creature> insects  = new ArrayList<Creature>();
@@ -60,94 +54,31 @@ public class MainWindow extends JFrame {
         setMenu();
         setSeedList();
 
-        docButton.setVerticalTextPosition(JToggleButton.BOTTOM);
-        docButton.setHorizontalTextPosition(JToggleButton.CENTER);
-        docButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (docButton.isSelected())
-                {
-                    missionButton.setSelected(false);
-                    centerLayout.show(centerPanel, "doc");
-                    if (seedListView.getSelectedValue() != null)
-                        docView.display((XMLLoadableElement) seedListView.getSelectedValue());
-                }
-                else
-                    centerLayout.show(centerPanel, "game");
-            }
-        });
-
-        missionButton.setVerticalTextPosition(JToggleButton.BOTTOM);
-        missionButton.setHorizontalTextPosition(JToggleButton.CENTER);
-        missionButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (missionButton.isSelected())
-                {
-                    docButton.setSelected(false);
-                    centerLayout.show(centerPanel, "mission");
-                    missionView.display(currentMission);
-                }
-                else
-                    centerLayout.show(centerPanel, "game");
-            }
-        });
-
-        levelBar.setValue(50);
+        levelBar.setValue(10);
         levelBar.setStringPainted(true);
-        levelBar.setString("Moitié du niveau !");
+        levelBar.setString("10 insectes");
 
-        centerPanel.setLayout(centerLayout);
-        centerPanel.add(gameView,     "game");
-        centerPanel.add(docView,      "doc");
-        centerPanel.add(missionView,  "mission");
-
-
-        GridBagLayout layout = new GridBagLayout();
-        GridBagConstraints c = new GridBagConstraints();
-        c.insets = new Insets(2, 2, 2, 2);
-        c.gridwidth  = 1;
-        c.gridheight = 1;
-        c.fill = GridBagConstraints.BOTH;
-
-        setLayout(layout);
-
-        layout.setConstraints(docButton, c);
-        getContentPane().add(docButton);
-
-        c.gridwidth  = 2;
-        c.gridheight = 4;
-        c.weightx = 1;
-        c.weighty = 1;
-        layout.setConstraints(centerPanel, c);
-        getContentPane().add(centerPanel);
-
-        c.gridwidth  = GridBagConstraints.REMAINDER;
-        c.gridheight = 4;
-        c.weightx = 0;
-        c.weighty = 0;
+        seedListView.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                if (KeyEvent.VK_ESCAPE == e.getKeyCode()) {
+                    statusBar.setText("Au revoir !");
+                    dispose();
+                }
+            }
+        });
+        seedListView.addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent e) {
+                Plant p = (Plant) seedListView.getSelectedValue();
+                statusBar.setText(p.name());
+            }
+        });
         JScrollPane s = new JScrollPane(seedListView);
         s.setPreferredSize(new Dimension(200, 400));
-        layout.setConstraints(s, c);
-        getContentPane().add(s);
 
-        c.gridy = 1;
-        c.gridx = 0;
-        c.gridwidth  = 1;
-        c.gridheight = 1;
-        layout.setConstraints(missionButton, c);
-        getContentPane().add(missionButton);
-
-        c.gridy = 2;
-        c.gridx = 0;
-        c.gridwidth  = 1;
-        c.gridheight = 2;
-        layout.setConstraints(levelBar, c);
-        getContentPane().add(levelBar);
-
-        c.gridy = 4;
-        c.gridwidth  = GridBagConstraints.REMAINDER;
-        c.gridheight = 1;
-        layout.setConstraints(statusBar, c);
-        getContentPane().add(statusBar);
+        getContentPane().add(gameView,  BorderLayout.CENTER);
+        getContentPane().add(s,         BorderLayout.EAST);
+        getContentPane().add(levelBar,  BorderLayout.WEST);
+        getContentPane().add(statusBar, BorderLayout.SOUTH);
 
         setJMenuBar(menuBar);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -159,6 +90,12 @@ public class MainWindow extends JFrame {
 
     private void run() {
         currentMission = missions.get(0);
+        play(currentMission.description());
+        statusBar.setText(currentMission.description());
+
+        seedListView.requestFocus();
+        seedListView.setSelectedIndex(0);
+
         int delay = 10000;
         ActionListener taskPerformer = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -169,6 +106,11 @@ public class MainWindow extends JFrame {
             }
         };
         new Timer(delay, taskPerformer).start();
+    }
+
+    private void play(String text) {
+        player.stop();
+        player.playText(text);
     }
 
     private void loadPlants() {
@@ -182,7 +124,16 @@ public class MainWindow extends JFrame {
     private void loadMissions() {
         Mission temp = new Mission("mission_01");
         missions.add(temp);
-        System.out.println(temp);
+    }
+
+    private void setMenu() {
+    }
+
+    private void setFullScreen() {
+        setUndecorated(true);
+        setSize(getToolkit().getScreenSize());
+        setLocationRelativeTo(null);
+        validate();
     }
 
     private void setSeedList() {
@@ -207,15 +158,5 @@ public class MainWindow extends JFrame {
 
             return this;
         }
-    }
-
-    private void setMenu() {
-    }
-
-    private void setFullScreen() {
-        setUndecorated(true);
-        setSize(getToolkit().getScreenSize());
-        setLocationRelativeTo(null);
-        validate();
     }
 }
