e1f2172341bb2cb8d4a8f2577c23c1d825452f5e
MattiSG <matti.schneiderghibaudo@gmail.com>
2009-03-31 18:48:22 +0000

    En ajoutant les fichiers du XMLparser c'est mieux :s
    git-svn-id: file:///Users/matti/Desktop/tmp-1/flore/svn/trunk@49 0452f256-4c2a-4938-993f-bf7dc213af01

---
+++ src/xml/XMLParser.java
@@ -0,0 +1,155 @@
+package xml;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+import javax.xml.xpath.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*; 
+import java.io.File;
+import java.net.URI;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
+
+/**This class provides a clean interface to the SAX Xpath engine.
+ *
+ *@author <a href="mailto:alcmene.gs@gmail.com">Matti Schneider-Ghibaudo</a>
+ *@version 0.1
+ */
+
+public class XMLParser {
+	
+	Document XML;
+	XPath xpath;
+	
+	public XMLParser(String path) {
+		this(new File(path));
+	}
+	
+	/**Loads the given XML file.
+	 *@param	file	the XML file to parse.
+	 *@throws	IllegalArgumentException	if the given file isn't parsable by this version of the parser.
+	 */	
+	public XMLParser(File file) {
+		try {
+			DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			XML = builder.parse(file);
+			xpath = XPathFactory.newInstance().newXPath();
+		} catch (javax.xml.parsers.ParserConfigurationException e) {
+			throw new RuntimeException("Erreur au chargement d'un fichier de description XML !\n" + e);
+		} catch (org.xml.sax.SAXException e) {
+			throw new RuntimeException("Erreur au chargement d'un fichier de description XML !\n" + e);
+		} catch (java.io.IOException e) {
+			throw new RuntimeException("Erreur au chargement d'un fichier de description XML !\n" + e);
+		}
+	}
+	
+	/**Returns the URI of the loaded XML file.
+	 */
+	public URI getDocumentURI() {
+		try {
+			return new URI(XML.getDocumentURI());
+		} catch (java.net.URISyntaxException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	/**Returns the result of the evaluation of an Xpath query as a String.
+	 */
+	public String get(String query) {
+		try {
+			return xpath.evaluate(query, XML);
+		} catch (javax.xml.xpath.XPathExpressionException e) {
+			throw new RuntimeException("Erreur (" + e + ") à l'analyse d'un fichier de description XML !\nRequête Xpath : " + query);
+		}
+	
+	}
+	
+	/**Returns the result of the evaluation of an Xpath query as a Double.
+	 */
+	public Double getDouble(String query) {
+		try {
+			return (Double) xpath.evaluate(query, XML, XPathConstants.NUMBER);
+		} catch (javax.xml.xpath.XPathExpressionException e) {
+			throw new RuntimeException("Erreur (" + e + ") à l'analyse d'un fichier de description XML !\nRequête Xpath : " + query);
+		}
+	}
+	
+	/**Returns the result of the evaluation of an Xpath query as a list of Nodes.
+	 *Every entry of the list will be one of the nodes described in the query parameter.
+	 */	
+	public List<Node> getNodes(String query) {
+		NodeList nodes = null;
+		try {
+			nodes = ((NodeList) xpath.evaluate(query, XML, XPathConstants.NODESET));
+		} catch (javax.xml.xpath.XPathExpressionException e) {
+			throw new RuntimeException("Erreur (" + e + ") à l'analyse d'un fichier de description XML !\nRequête Xpath : " + query);
+		}
+		
+		List<Node> values = new ArrayList<Node>(nodes.getLength());
+		for (int i = 0; i < nodes.getLength(); i++)
+			values.add(nodes.item(i));
+		
+		return values;
+	}
+	
+	/**Returns the result of the evaluation of an Xpath query as a list of Strings.
+	 *Every entry of the list will be the value of one of the children of the node described in the query parameter.
+	 *@param	query	an Xpath expression targeting one node whose children will be used as values in the list (as opposed to the children themselves)
+	 */
+	public List<String> getValues(String query) {
+//		NodeList nodes = null;
+//		try {
+//			nodes = ((NodeList) xpath.evaluate(query, XML, XPathConstants.NODE));
+//		} catch (javax.xml.xpath.XPathExpressionException e) {
+//			throw new RuntimeException("Erreur (" + e + ") à l'analyse d'un fichier de description XML !\nRequête Xpath : " + query);
+//		}
+//		
+//		List<String> values = new ArrayList<String>(nodes.getLength());
+//		for (int i = 0; i < nodes.getLength(); i++) {
+//			Node node = nodes.item(i);
+//			if (node.getNodeType() == Node.ELEMENT_NODE) //otherwise we load everything, including comments and pure text
+//				values.add(node.getTextContent());
+//		}
+//		
+//		return values;
+		List<Node> nodes = getNodes(query + "/*");
+		List<String> values = new ArrayList<String>(nodes.size());
+		for (Node node : nodes) {
+			if (node.getNodeType() == Node.ELEMENT_NODE) //otherwise we load everything, including comments and pure text
+				values.add(node.getTextContent());
+		}
+		return values;
+	}
+	
+	/**Returns the result of the evaluation of an Xpath query as a map of Strings to lists of Strings.
+	 *An entry in the map will have its key be the tag of a node and its value be a list of all values inside it.
+	 *@param	query	an Xpath expression targeting one node whose children will be used as keys for the map (as opposed to the children themselves)
+	 *@see	getValues
+	 *@throws	RuntimeException	if one of the nodes from the query doesn't have children.
+	 */
+	public Map<String, List<String>> getListsMap(String query) {
+		Map<String, List<String>> result = new HashMap<String, List<String>>();
+		List<Node> keys = getNodes(query + "/*");
+		
+//		System.out.println("getListsMap(" + query + ") -> size of first-level parsing is " + keys.size());
+		
+		for (Node key : keys) {
+//			System.out.println("\"" + key.getTextContent() + "\" is of type " + key.getNodeType());
+			if (! key.hasChildNodes())
+				throw new RuntimeException("The node \"" + key.getNodeName() + "\" has no children !");
+			
+//			System.out.println("getListsMap(" + query + ") -> getValues(" + query + "/" + key.getLocalName() + "/* )");
+			
+			result.put(key.getNodeName(), getValues(query + "/" + key.getNodeName()));
+		}
+		return result;
+	}
+}
---
+++ test/xml/XMLParserTest.java
@@ -0,0 +1,98 @@
+/**A set of tests for the XMLParser class.
+*@author <a href="mailto:alcmene.gs@gmail.com">Matti Schneider-Ghibaudo</a>
+*@version 0.1
+*/
+
+package test.xml;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.io.File;
+
+import xml.XMLParser;
+import test.element.XMLLoadableElementTest;
+import test.element.plant.PlantTest;
+
+public class XMLParserTest {
+	
+	private static String testFile;
+	
+	private static XMLLoadableElementTest expectedValuesHolder;
+	private XMLParser subject;
+	
+	private final static String EXPR_VERSION = "//id/../@version";
+	private final static String EXPR_ID = "//id";
+	private final static String EXPR_NAME = "//name[@lang='fr']";
+	private final static String EXPR_LISTSMAP_TEST = "//assets[@type='standard']";
+	private final static String EXPR_LIST_TEST = EXPR_LISTSMAP_TEST + "/shaft";
+	private final static String EXPR_DESCRIPTION = "//description";
+	
+	@BeforeClass
+	public static void classSetUp() {
+		expectedValuesHolder = new PlantTest();
+		expectedValuesHolder.setUp();
+		File tmp = new File("../ressources/elements/" + expectedValuesHolder.EXPECTED_ID + "/description.xml"); //WTF ?!? Well, Java seems to be unable to resolve a relative path without this... Don't ask me !
+		testFile = tmp.getAbsolutePath();
+		try {
+			File f = new File(testFile);
+			if (! f.exists())
+				throw new java.io.IOException();
+		} catch(java.io.IOException e) {
+			fail("The testing file \"" + testFile + "\"doesn't exist !\nPlease check the PlantTest file.");
+		}
+	}
+	
+	@Before
+	public void setUp() {
+		subject = new XMLParser(testFile);
+	}
+	
+	@Test
+	public void XMLParserInitTest() {
+		assertNotNull("String constructor created a null object !", subject);
+		subject = new XMLParser(new File(testFile));
+		assertNotNull("File constructor created a null object !", subject);
+	}
+	
+	@Test
+	public void stringParsingTest() {
+		assertEquals(expectedValuesHolder.EXPECTED_ID, subject.get(EXPR_ID));
+		assertEquals(expectedValuesHolder.EXPECTED_NAME, subject.get(EXPR_NAME));
+		assertEquals(expectedValuesHolder.EXPECTED_DESCRIPTION, subject.get(EXPR_DESCRIPTION));
+	}
+	
+	@Test
+	public void doubleParsingTest() {
+		assertNotNull(subject.getDouble(EXPR_VERSION));
+	}
+	
+	public void checkList(List<String> list) {
+		assertFalse(list.isEmpty());
+		for (String s : list) {
+			System.out.println("\"" + s + "\"");
+			assertNotNull(s);
+			assertFalse("Parsed string is empty !", s.length() == 0);
+		}
+	}
+	
+	@Test
+	public void listParsingTest() {
+		checkList(subject.getValues(EXPR_LIST_TEST));
+	}
+	
+	@Test
+	public void listsMapParsingTest() {
+		Map<String, List<String>> map = subject.getListsMap(EXPR_LISTSMAP_TEST);
+		assertFalse("getListsMap returned an empty map for the Xpath expression \"" + EXPR_LISTSMAP_TEST + "\" !", map.isEmpty());
+		for (List<String> list : map.values())
+			checkList(list);
+	}
+}
