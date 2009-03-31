ef664ba35b4f719e0be8d277f3cbe91021a6352e
RomainGiraud <giraud.romain@gmail.com>
2009-03-31 19:43:50 +0000

    Compatibilité de caca pour le build.
    git-svn-id: file:///Users/matti/Desktop/tmp-1/flore/svn/trunk@51 0452f256-4c2a-4938-993f-bf7dc213af01

--- src/element/mission/Mission.java
+++ src/element/mission/Mission.java
@@ -2,12 +2,20 @@ package element.mission;
 
 import element.XMLLoadableElement;
 
+import java.util.Map;
+import java.util.List;
+import java.awt.image.BufferedImage;
+
 public class Mission extends XMLLoadableElement {
 	private final static double PARSER_VERSION = 0;
     private final static String[] ASSETS_NAMES = new String[0];
 
     public Mission(String ID) {
         load(ID);
+    }
+
+	protected Map<String, List<BufferedImage>> loadAssets(String query) {
+        return null;
     }
 
 	public double parserVersion() {
