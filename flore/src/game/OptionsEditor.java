d70bb588086b3c6860dd55d173d82e854d06f3cc
RomainGiraud <giraud.romain@gmail.com>
2009-05-20 20:24:08 +0000

    Éditeur d'options (juste les principales)
    git-svn-id: file:///Users/matti/Desktop/tmp-1/flore/svn/trunk@170 0452f256-4c2a-4938-993f-bf7dc213af01

--- flore/src/game/OptionsEditor.java
+++ flore/src/game/OptionsEditor.java
@@ -7,19 +7,117 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JSpinner;
 import javax.swing.BoxLayout;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.SpinnerNumberModel;
 
 import java.awt.Dimension;
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowAdapter;
 
 public class OptionsEditor extends JFrame {
+    private JPanel centerPanel = new JPanel();
+
     public OptionsEditor() {
-        JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
 
+        setFontSizeEditor();
+        setCreatureSpeedEditor();
+        setCreatureZoomEditor();
+        setPlantZoomEditor();
+        setMissionZoomEditor();
 
-        JPanel fontSizeEditor = new JPanel();
-        fontSizeEditor.setLayout(new BoxLayout(fontSizeEditor, BoxLayout.X_AXIS));
+        getContentPane().add(centerPanel, BorderLayout.CENTER);
 
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setFullScreen();
+        setVisible(true);
+    }
+
+    private void addEditor(String label, Component comp) {
+        JPanel editor = new JPanel();
+        editor.setLayout(new BoxLayout(editor, BoxLayout.X_AXIS));
+        editor.add(new JLabel(label));
+        editor.add(comp);
+        centerPanel.add(editor);
+    }
+
+    private void setCreatureSpeedEditor() {
+        JSpinner spinner = new JSpinner();
+        JSpinner.NumberEditor spinnerEditor = new JSpinner.NumberEditor(spinner);
+        spinner.setEditor(spinnerEditor);
+        spinnerEditor.getModel().setMinimum(0);
+        spinnerEditor.getModel().setMaximum(20);
+        spinnerEditor.getModel().setStepSize(1);
+        spinnerEditor.getModel().setValue(GlobalProperties.getDouble("Creature_Speed"));
+
+        spinnerEditor.getModel().addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                GlobalProperties.set("Creature_Speed", ""+((SpinnerNumberModel)e.getSource()).getValue());
+            }
+        });
+
+        addEditor("Vitesse de la créature", spinner);
+    }
+
+    private void setCreatureZoomEditor() {
+        JSpinner spinner = new JSpinner();
+        JSpinner.NumberEditor spinnerEditor = new JSpinner.NumberEditor(spinner);
+        spinner.setEditor(spinnerEditor);
+        spinnerEditor.getModel().setMinimum(0);
+        spinnerEditor.getModel().setMaximum(10);
+        spinnerEditor.getModel().setStepSize(0.1);
+        spinnerEditor.getModel().setValue(GlobalProperties.getDouble("Creature" + GlobalProperties.ZOOM_SUFFIX));
+
+        spinnerEditor.getModel().addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                GlobalProperties.set("Creature"+GlobalProperties.ZOOM_SUFFIX, ""+((SpinnerNumberModel)e.getSource()).getValue());
+            }
+        });
+
+        addEditor("Zoom de la créature", spinner);
+    }
+
+    private void setPlantZoomEditor() {
+        JSpinner spinner = new JSpinner();
+        JSpinner.NumberEditor spinnerEditor = new JSpinner.NumberEditor(spinner);
+        spinner.setEditor(spinnerEditor);
+        spinnerEditor.getModel().setMinimum(0);
+        spinnerEditor.getModel().setMaximum(10);
+        spinnerEditor.getModel().setStepSize(0.1);
+        spinnerEditor.getModel().setValue(GlobalProperties.getDouble("Plant" + GlobalProperties.ZOOM_SUFFIX));
+
+        spinnerEditor.getModel().addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                GlobalProperties.set("Plant"+GlobalProperties.ZOOM_SUFFIX, ""+((SpinnerNumberModel)e.getSource()).getValue());
+            }
+        });
+
+        addEditor("Zoom de la plante", spinner);
+    }
+
+    private void setMissionZoomEditor() {
+        JSpinner spinner = new JSpinner();
+        JSpinner.NumberEditor spinnerEditor = new JSpinner.NumberEditor(spinner);
+        spinner.setEditor(spinnerEditor);
+        spinnerEditor.getModel().setMinimum(0);
+        spinnerEditor.getModel().setMaximum(10);
+        spinnerEditor.getModel().setStepSize(0.1);
+        spinnerEditor.getModel().setValue(GlobalProperties.getDouble("Mission" + GlobalProperties.ZOOM_SUFFIX));
+
+        spinnerEditor.getModel().addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                GlobalProperties.set("Mission"+GlobalProperties.ZOOM_SUFFIX, ""+((SpinnerNumberModel)e.getSource()).getValue());
+            }
+        });
+
+        addEditor("Zoom de la mission", spinner);
+    }
+
+
+    private void setFontSizeEditor() {
         JSpinner spinner = new JSpinner();
         JSpinner.NumberEditor spinnerEditor = new JSpinner.NumberEditor(spinner);
         spinner.setEditor(spinnerEditor);
@@ -28,18 +126,13 @@ public class OptionsEditor extends JFrame {
         spinnerEditor.getModel().setStepSize(1);
         spinnerEditor.getModel().setValue(GlobalProperties.getInteger("font_size"));
 
-        fontSizeEditor.add(new JLabel("Taille de la police")); 
-        fontSizeEditor.add(spinner);
+        spinnerEditor.getModel().addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                GlobalProperties.set("font_size", ""+((SpinnerNumberModel)e.getSource()).getValue());
+            }
+        });
 
-        fontSizeEditor.setPreferredSize(new Dimension(400, 100));
-
-        centerPanel.add(fontSizeEditor);
-        
-        getContentPane().add(centerPanel, BorderLayout.CENTER);
-
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setFullScreen();
-        setVisible(true);
+        addEditor("Taille de la police", spinner);
     }
 
     private void setFullScreen() {
